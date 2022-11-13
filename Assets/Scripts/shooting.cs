9f1fbae013f1803ca9fbf42670bc322015c39209
unknown <rotrast@gmail.com>
2022-11-13 17:52:17 +0200

    Add power bar and cleaned the code

--- Assets/Scripts/shooting.cs
+++ Assets/Scripts/shooting.cs
@@ -2,31 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class shooting : MonoBehaviour
+public class Shooting : MonoBehaviour
 {
-    // Start is called before the first frame update
-   
-    
-        public Transform target;
 
-        public float Force;
-        
-    
+    public Transform target;
+
+    public static float power;   
 
     // Update is called once per frame
     void Update()
     {
-    if(Input.GetKeyDown(KeyCode.Space))
-    {
-        shoot();
-    }    
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            shoot();
+        }         
     }
 
 
     void shoot()
-{
-    Vector3 Shoot = (target.position - this.transform.position).normalized;
-    GetComponent<Rigidbody>().AddForce(Shoot * Force + new Vector3(0, 3f, 0), ForceMode.Impulse);
-}
+    {
+        Vector3 Shoot = (target.position - this.transform.position).normalized;
+        GetComponent<Rigidbody>().AddForce(Shoot * power + new Vector3(0, 3f, 0), ForceMode.Impulse);
+    }
 }

69db5d6b80b8bf5cb0c1cb4e4af8419dd4cf9e61
Capmare Alex-Mihai <73711932+CapmareAlex@users.noreply.github.com>
2022-11-20 21:42:51 +0200

    Add Goal Message

---
+++ Assets/Scripts/CollisionWall.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionWall : MonoBehaviour
+{
+
+    public GameObject Message;
+    void start()
+    {
+        Message.SetActive(false);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            print("enter");
+            Message.SetActive(true);
+
+        }
+    }
+    void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            print("stay");
+            Message.SetActive(true);
+
+        }
+    }
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Wall")
+        {
+            print("exit");
+            Message.SetActive(true);
+
+        }
+    }
+}

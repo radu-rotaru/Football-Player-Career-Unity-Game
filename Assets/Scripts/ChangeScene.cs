ac181792871deb4b8b7b557dbb2daf56ad3b5859
aleksandeerG <82213474+aleksandeerG@users.noreply.github.com>
2022-12-04 23:09:45 +0200

    Added a play menu

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
